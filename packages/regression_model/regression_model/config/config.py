ac38572972e62c390f031fbb1be25ea33a6fbb29
aironrahul <aironrahul@gmail.com>
2019-11-28 12:33:57 +0530

    something23

--- packages/regression_model/regression_model/config/config.py
+++ packages/regression_model/regression_model/config/config.py
@@ -23,6 +23,17 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 from regression_model.processing.errors import InvalidModelInputError
 
+from sklearn.linear_model import Lasso
+#from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+#from regression_model.processing import preprocessors as pp
+#from regression_model.processing import features
+#from regression_model.config import config
+
+#import logging
+
+
 #from regression_model.config import config
 #from regression_model import __version__ as _version
 
@@ -190,56 +201,6 @@ def remove_old_pipelines(*, files_to_keep) -> None:
             model_file.unlink()
 
 
-
-
-
-_logger = logging.getLogger(__name__)
-
-
-def load_dataset(*, file_name: str
-                 ) -> pd.DataFrame:
-    _data = pd.read_csv(f'{DATASET_DIR}/{file_name}')
-    return _data
-
-
-def save_pipeline(*, pipeline_to_persist) -> None:
-    """Persist the pipeline.
-
-    Saves the versioned model, and overwrites any previous
-    saved models. This ensures that when the package is
-    published, there is only one trained model that can be
-    called, and we know exactly how it was built.
-    """
-
-    # Prepare versioned save file name
-    save_file_name = f'{PIPELINE_SAVE_FILE}{__version__}.pkl'
-    save_path = TRAINED_MODEL_DIR / save_file_name
-
-    remove_old_pipelines(files_to_keep=save_file_name)
-    joblib.dump(pipeline_to_persist, save_path)
-    _logger.info(f'saved pipeline: {save_file_name}')
-
-
-def load_pipeline(*, file_name: str
-                  ) -> Pipeline:
-    """Load a persisted pipeline."""
-
-    file_path = TRAINED_MODEL_DIR / file_name
-    trained_model = joblib.load(filename=file_path)
-    return trained_model
-
-
-def remove_old_pipelines(*, files_to_keep) -> None:
-    """
-    Remove old model pipelines.
-    This is to ensure there is a simple one-to-one
-    mapping between the package version and the model
-    version to be imported and used by other applications.
-    """
-
-    for model_file in TRAINED_MODEL_DIR.iterdir():
-        if model_file.name not in [files_to_keep, '__init__.py']:
-            model_file.unlink()
 
 
 
@@ -435,3 +396,34 @@ class LogTransformer(BaseEstimator, TransformerMixin):
             X[feature] = np.log(X[feature])
 
         return X
+
+
+
+
+_logger = logging.getLogger(__name__)
+
+
+price_pipe = Pipeline(
+    [
+        ('categorical_imputer',
+            CategoricalImputer(variables=CATEGORICAL_VARS_WITH_NA)),
+        ('numerical_inputer',
+            NumericalImputer(variables=NUMERICAL_VARS_WITH_NA)),
+        ('temporal_variable',
+            TemporalVariableEstimator(
+                variables=TEMPORAL_VARS,
+                reference_variable=DROP_FEATURES)),
+        ('rare_label_encoder',
+            RareLabelCategoricalEncoder(
+                tol=0.01,
+                variables=CATEGORICAL_VARS)),
+        ('categorical_encoder',
+            CategoricalEncoder(variables=CATEGORICAL_VARS)),
+        ('log_transformer',
+            LogTransformer(variables=NUMERICALS_LOG_VARS)),
+        ('drop_features',
+            DropUnecessaryFeatures(variables_to_drop=DROP_FEATURES)),
+        ('scaler', MinMaxScaler()),
+        ('Linear_model', Lasso(alpha=0.005, random_state=0))
+    ]
+)
