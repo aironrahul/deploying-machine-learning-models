c5874bb402428aae6da63745a503fdf147d9fd23
aironrahul <aironrahul@gmail.com>
2019-11-28 19:20:26 +0530

    something45

--- packages/regression_model/regression_model/config/config.py
+++ packages/regression_model/regression_model/config/config.py
@@ -36,6 +36,16 @@ from regression_model.processing.validation import validate_inputs
 
 import logging
 
+#import numpy as np
+from sklearn.model_selection import train_test_split
+
+#from regression_model.config import pipeline
+#from regression_model.config import (load_dataset, save_pipeline)
+#from regression_model.config import config
+from regression_model import __version__ as _version
+
+import logging
+
 
 #from regression_model.processing import preprocessors as pp
 #from regression_model.processing import features
@@ -444,6 +454,40 @@ _logger = logging.getLogger(__name__)
 
 pipeline_file_name = f'{PIPELINE_SAVE_FILE}{__version__}.pkl'
 _price_pipe = load_pipeline(file_name=pipeline_file_name)
+
+
+
+_logger = logging.getLogger(__name__)
+
+
+def run_training() -> None:
+    """Train the model."""
+
+    # read training data
+    data = load_dataset(file_name=TRAINING_DATA_FILE)
+
+    # divide train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[FEATURES],
+        data[TARGET],
+        test_size=0.1,
+        random_state=0)  # we are setting the seed here
+
+    # transform the target
+    y_train = np.log(y_train)
+    y_test = np.log(y_test)
+
+    _price_pipe.fit(X_train[FEATURES],
+                            y_train)
+
+    _logger.info(f'saving model version: {_version}')
+    save_pipeline(pipeline_to_persist=_price_pipe)
+
+
+#if __name__ == '__main__':
+ #   run_training()
+
+
 
 
 def make_prediction(*, input_data) -> dict:
